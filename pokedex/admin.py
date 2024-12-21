6533cb514046a5287596ef086b9e40fee7b4623d
Said <msnavarretm@puce.edu.ec>
2024-12-21 09:25:39 -0500

    Trainer model and view added - Lab8.2

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 @admin.register(Pokemon)
+@admin.register(Trainer)
 
 class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+class TrainerAdmin(admin.ModelAdmin):
     pass
--- pokedex/models.py
+++ pokedex/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 class Pokemon(models.Model):
     name = models.CharField(max_length=30, null=False)
-    type = models.CharField(max_length=30, null=False)
+    POKEMON_TYPES = {('A', 'Agua'),
+                     ('F', 'Fuego'),
+                     ('T', 'Tierra'),
+                     ('P', 'Planta'),
+                     ('E', 'Electrico'),
+                     ('R', 'Roca')}
+    type = models.CharField(max_length=30, null=False, choices=POKEMON_TYPES)
     weight = models.DecimalField(max_digits=6, decimal_places=4)
     height = models.DecimalField(max_digits=6, decimal_places=4)
     
@@ -15,6 +21,10 @@ class Trainer(models.Model):
     last_name = models.CharField(max_length=30, null=False)
     level = models.PositiveSmallIntegerField(null=False)
     birth_date = models.DateField(null=False)
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+    
 
 
     
